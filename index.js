270fb1ac1fa19dc94a6dddbfb837b4e73614106c
Charl <94119475+Charl1980@users.noreply.github.com>
2022-07-12 13:49:24 +0200

    Update CORS

--- index.js
+++ index.js
@@ -28,27 +28,6 @@ mongoose.connect(process.env.CONNECTION_URI, {
   useUnifiedTopology: true
 });
 
-//CORS implementation
-const cors = require('cors');
-let allowedOrigins = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'https://myflix-movies1980.herokuapp.com/',
-  'https://www.themoviedb.org/',
-  'Access-Control-Allow-Origin'
-];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 //Middleware functions
 app.use(morgan('common'));
 app.use(express.static('public'));
@@ -56,6 +35,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+//CORS implementation
+const cors = require('cors');
+app.use(cors());
 
 //IMPORT auth.js file
 let auth = require('./auth')(app);
